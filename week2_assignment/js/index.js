c25cb6af8e67aef1b3fa874afe197ce9355adaad
hanseo <yds06206@gmail.com>
2024-10-27 15:15:42 +0900

    feat: 필터링 구현

--- week2_assignment/js/index.js
+++ week2_assignment/js/index.js
@@ -22,11 +22,47 @@ const addTableRow = (item) => {
 };
 
 const displayTableList = (items) => {
-  const TableRow = document.querySelector(".results_items");
-  TableRow.innerHTML = items.map((item) => addTableRow(item)).join("");
+  const TableRowWrapper = document.querySelector(".results_items");
+  TableRowWrapper.innerHTML = items.map((item) => addTableRow(item)).join("");
 };
 
 //최초 렌더링
 document.addEventListener("DOMContentLoaded", () => {
   displayTableList(membersData);
+});
+
+//필터 함수
+const filterMembers = (filters) => {
+  return membersData.filter((member) => {
+    return Object.keys(filters).every((key) => {
+      return filters[key] ? filters[key] === member[key] : true;
+    });
+  });
+};
+
+let filters = {};
+
+document.querySelector(".input_gender").addEventListener("change", (event) => {
+  filters.gender = event.target.value;
+});
+
+document.querySelector(".input_role").addEventListener("change", (event) => {
+  filters.role = event.target.value;
+});
+
+//검색 버튼 클릭시 필터 목록 렌더링
+document.querySelector(".search_btn").addEventListener("click", (event) => {
+  filters = {
+    name: document.querySelector(".input_name").value,
+    englishName: document.querySelector(".input_engName").value,
+    github: document.querySelector(".input_github").value,
+    gender: document.querySelector(".input_gender").value,
+    role: document.querySelector(".input_role").value,
+    firstWeekGroup: document.querySelector(".input_week1").value,
+    secondWeekGroup: document.querySelector(".input_week2").value,
+  };
+
+  const filteredMembers = filterMembers(filters);
+
+  displayTableList(filteredMembers);
 });
