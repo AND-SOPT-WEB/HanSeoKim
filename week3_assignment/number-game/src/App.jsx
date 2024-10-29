812465dfafdf98587dec94c96d41a7e2a2b88cb2
hanseo <yds06206@gmail.com>
2024-10-30 00:18:00 +0900

    init: 필요없는 파일 삭제, global style 지정

--- week3_assignment/number-game/src/App.jsx
+++ week3_assignment/number-game/src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import MainLayout from "./components/layout/MainLayout";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { Global } from "@emotion/react";
 
 function App() {
-  return <></>;
+  return (
+    <Global css={GlobalStyle}>
+      <MainLayout></MainLayout>
+    </Global>
+  );
 }
 
 export default App;
---
+++ week3_assignment/number-game/src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyle = css`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  ul,
+  ol,
+  li {
+    list-style: none;
+  }
+`;
