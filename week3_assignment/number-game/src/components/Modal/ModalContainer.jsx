dd66751eb70f030d554257179e040b249f93a40d
hanseo <yds06206@gmail.com>
2024-11-08 22:31:27 +0900

    style: 모달 백드롭 수정

--- week3_assignment/number-game/src/components/Modal/ModalContainer.jsx
+++ week3_assignment/number-game/src/components/Modal/ModalContainer.jsx
@@ -1,9 +1,24 @@
 import { createPortal } from "react-dom";
 
-import { backdropStyle } from "./ModalContent.style";
-
 const ModalContainer = ({ children }) => {
-  return createPortal(<div css={backdropStyle}>{children}</div>, document.body);
+  return createPortal(
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>,
+    document.body
+  );
 };
 
 export default ModalContainer;
--- week3_assignment/number-game/src/components/Modal/ModalContent.style.js
+++ week3_assignment/number-game/src/components/Modal/ModalContent.style.js
@@ -33,15 +33,3 @@ export const buttonStyle = css`
   color: black;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 `;
-
-export const backdropStyle = css({
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-});
