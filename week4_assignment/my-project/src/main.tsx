ad30e165bb4237aadb7b7b5fc64a386d51791a86
hanseo <yds06206@gmail.com>
2024-11-10 20:15:44 +0900

    style: theme, style 설정

--- week4_assignment/my-project/src/main.tsx
+++ week4_assignment/my-project/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { Global } from "@emotion/react";
 import App from "./App.tsx";
+import { GlobalStyle } from "./styles/GlobalStyle.ts";
+import { Theme } from "./styles/theme.ts";
+import { ThemeProvider } from "@emotion/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <Global styles={GlobalStyle}></Global>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ week4_assignment/my-project/src/styles/theme.ts
@@ -0,0 +1,30 @@
+const color = {
+  white: "white",
+  lightGray_1: "#B2C6E4",
+  lightGray_2: "#B2C6E4",
+  lightGray_3: "#707D90",
+  gray: "#5A6473",
+  darkGray_1: "#444B57",
+  darkGray_2: "#2D323A",
+  black: "black",
+};
+
+const text = {
+  large: {
+    fontSize: "16px",
+    lineHeight: "19px",
+  },
+  medium: {
+    fontSize: "14px",
+    lineHeight: "17px",
+  },
+  small: {
+    fontSize: "12px",
+    lineHeight: "14px",
+  },
+};
+
+export const Theme = {
+  color,
+  text,
+};
